4338423a4847777c7e600215a398e040504ed72c
MarkyMark <mgelabert3@gmail.com>
2018-03-12 00:25:53 +0100

    update

--- socketEvents.js
+++ socketEvents.js
@@ -1,20 +1,5 @@
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    console.log('user connected');
-    socket.emit('ACTION', {
-      type: 'SET_NEW_DATE',
-      data: {
-        time: new Date()
-      }
-    });
-    socket.on('SOCKET__CONNECT', async (data) => {
-      console.log('User connected to socket.');
-    })
-
-    socket.on('SEND_MESSAGE', async (data) => {
-      console.log('mofoka');
-    })
-
     setInterval(() => {
       console.log('action fired SET NEW DATE');
       socket.emit('ACTION', {
@@ -23,6 +8,22 @@ module.exports = (io) => {
           time: new Date()
         }
       });
-    }, 1000);
+    }, 5000);
+    console.log('user connected');
+    socket.emit('ACTION', {
+      type: 'SET_NEW_DATE',
+      data: {
+        time: new Date()
+      }
+    });
+    socket.on('SOCKET__DISCONNECT', async (data) => {
+      console.log('user logged off');
+    })
+
+    socket.on('SEND_MESSAGE', async (data) => {
+      console.log('mofoka');
+    })
+
+
   })
 }
