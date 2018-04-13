7e61ce601ef625f3506b74d6744a21a1001a69f7
MarkyMark <mgelabert3@gmail.com>
2018-04-13 23:41:59 +0200

    update

--- socketEvents/socketEvents.js
+++ socketEvents/socketEvents.js
@@ -1,28 +1,25 @@
-module.exports = (io) => {
-  io.on('connection', (socket) => {
+module.exports = io => {
+  io.on("connection", socket => {
+    socket.on("SOCKET__CONNECT", async data => {
+      const { room } = data;
+      console.log(data, "this room");
+      socket.join(room);
+      console.log("user joined");
+    });
 
+    socket.on("SOCKET__DISCONNECT", async data => {
+      console.log("user logged off");
+    });
+    socket.on("SOCKET__MESSAGE", data => {
+      console.log(data);
+      socket.broadcast.to(data.room).emit("ACTION", {
+        type: "REFRESH_MESSAGES",
+        data
+      });
+    });
 
-    socket.on('SOCKET__CONNECT', async (data) => {
-      const {room} = data
-      console.log(data, 'this room');
-      socket.join(room)
-      console.log('user joined');
-    })
-
-    socket.on('SOCKET__DISCONNECT', async (data) => {
-      console.log('user logged off');
-    })
-    socket.on('SOCKET__MESSAGE', (data) => {
-        socket.broadcast.to(data.room).emit('ACTION', {
-          type: 'REFRESH_MESSAGES',
-          data
-        });
-    })
-
-    socket.on('SEND_MESSAGE', async (data) => {
-      console.log('');
-    })
-
-
-  })
-}
+    socket.on("SEND_MESSAGE", async data => {
+      console.log("");
+    });
+  });
+};
